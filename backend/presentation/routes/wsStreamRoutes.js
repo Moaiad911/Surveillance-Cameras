486923540e18ca1d9338151bca5d9d32c98c7c8a
superior <superiorshipet@gmail.com>
2026-05-07 11:37:12 +0300

    ok now finally the back works with the ai model

--- backend/presentation/routes/wsStreamRoutes.js
+++ backend/presentation/routes/wsStreamRoutes.js
@@ -1,7 +1,7 @@
 const { WebSocketServer } = require('ws');
 const jwt = require('jsonwebtoken');
 const CameraModel = require('../../infrastructure/models/CameraModel');
-const { startWSStream } = require('../../infrastructure/repositories/wsStreamService');
+const { startWSStream } = require('../../infrastructure/wsStreamService');
 
 const setupWSStream = (server) => {
   const wss = new WebSocketServer({ 
